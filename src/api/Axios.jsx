7dcd505f7bf1f10177efca221b3aed5f9eb24873
Kaushall-raut <KaushalRaut.code@gmail.com>
2025-06-21 17:07:17 +0530

    removing api key

--- src/api/Axios.jsx
+++ src/api/Axios.jsx
@@ -3,16 +3,14 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_KEY,
 });
-console.log("key", import.meta.env.VITE_API_KEY);
+
 
 //! fetching api data
 
 export const getData = async (from, to, amount) => {
   try {
     const res = await api.get(`/pair/${from}/${to}/${amount}`);
-    console.log("====================================");
-    console.log(res.data);
-    console.log("====================================");
+ 
     return res.data.conversion_result;
   } catch (error) {
     console.log(error.message);
